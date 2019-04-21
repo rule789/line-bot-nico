43444b816f9d2c5407b3f208cbaf9525c3acd6f1
Nicole Chia <ganew12345@gmail.com>
2019-04-21 19:39:13 +0800

    update

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ bot.on('message', function (event) {
 
 
 // Bot所監聽的webhook路徑與port
-bot.listen('/linewebhook', 3000, function () {
+app.listen( process.env.PORT || 3000 , function () {
     console.log('[BOT已準備就緒]');
 });
 
