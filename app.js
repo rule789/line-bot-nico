2ef4aec859624d3690f8b94773c1943e8251b490
Nicole Chia <ganew12345@gmail.com>
2019-04-21 18:51:57 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+
+
+// 引用linebot SDK
+const linebot = require('linebot');
+
+// 用於辨識Line Channel的資訊
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESSTOKEN
+});
+
+const linebotParser = bot.parser();
+
+app.post('/linewebhook', linebotParser);
+
+// 當有人傳送訊息給Bot時
+bot.on('message', function (event) {
+  console.log(event);
+    // event.message.text是使用者傳給bot的訊息
+  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
+  event.reply('你說'+ event.message.text).then(function (data) {
+        // 當訊息成功回傳後的處理
+    console.log('Success', data);
+  }).catch(function (error) {
+        // 當訊息回傳失敗後的處理
+    console.log('Error', error);
+  });
+});
+
+
+// Bot所監聽的webhook路徑與port
+bot.listen('/linewebhook', 3000, function () {
+    console.log('[BOT已準備就緒]');
+});
+
+
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
